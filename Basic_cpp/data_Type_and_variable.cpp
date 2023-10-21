d1d4c28330dbf2fec63461b7d8e267172f9674e6
TusharJagi <tushar.j@fnp.com>
2023-10-21 22:15:01 +0530

    basic cpp

---
+++ Basic_cpp/data_Type_and_variable.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i = 123;
+  cout << i << endl;
+
+  char c = 'v';
+  cout << c << endl;
+
+  bool b = false;
+  cout << b << endl;
+
+  float f = 1.234;
+  cout << f << endl;
+
+  double d = 12.32121321323;
+  cout << d << endl;
+
+  int size = sizeof(i);
+  cout << "Size of variables:" << size << endl;
+
+} 
---
+++ Basic_cpp/type_casting.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+  int a = 'a';
+  cout << a << endl;
+
+  char ch = 98;
+  cout << ch << endl;
+
+  char cha = 123456;
+  cout << cha << endl; 
+
+  int i = -2;
+  cout << i << endl;
+
+  unsigned int u = 112;
+  cout << u << endl; 
+}
