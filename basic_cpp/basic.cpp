56f59c5b6ebd4005586b6494afabd163ba635b65
Tushar Jagi <tushar.j@fnp.com>
2024-09-21 13:40:26 +0530

    Basic

---
+++ basic_cpp/basic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int file_size = 100;
+  int counter = 0;
+  double sales = 9.99;
+
+  const double pi = 3.14;
+
+  int x = 10;
+  int y = 5;
+
+  
+  return 0;
+}
